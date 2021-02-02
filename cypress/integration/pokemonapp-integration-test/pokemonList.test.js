c1dab62000910eb13c0589e04b08c350c17356ed
carinama1 <jarpani30@gmail.com>
2021-02-03 00:33:48 +0700

    asd

--- cypress/integration/pokemonapp-integration-test/pokemonList.test.js
+++ cypress/integration/pokemonapp-integration-test/pokemonList.test.js
@@ -6,16 +6,4 @@ describe("Handle Catch The Correct Pokemon", () => {
     cy.get("div").contains(pokemonName).click();
     cy.get("div").contains(pokemonName);
   });
-
-  it("Capturing the correct pokemon", () => {
-    cy.visit("http://localhost:3000/");
-
-    cy.get("div").contains(pokemonName).click();
-    cy.get(".poke-ball").click();
-    if (cy.get("div").contains("GOTTEM!")) {
-      cy.get("div").contains("Gotta catch em' all!");
-    } else {
-      cy.get("div").contains("OH NO!");
-    }
-  });
 });
